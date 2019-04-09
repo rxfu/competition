8e00497b678460bdc0a869baf0fbd218b8e94aa9
Stephen Fu <stephen.frx@gmail.com>
2019-04-09 10:23:27 +0800

    add readme

--- config/setting.php
+++ config/setting.php
@@ -2,11 +2,11 @@
 
 return [
     'base' => '/laradmin',
-    'name' => 'Laravel Admin',
-    'slug' => 'LA',
-    'keywords' => 'Laravel, Vue.js, Bootstrap, Admin page',
-    'description' => 'Administration boilerplate using Laravel',
+    'name' => '广西高校教师教学技能比赛管理系统',
+    'slug' => '教技',
+    'keywords' => 'Laravel, Bootstrap, 后台管理',
+    'description' => '广西高校教师教学技能比赛选手信息管理及评分',
     'author' => 'Fu Rongxin',
-    'copyright' => 'Fu Rongxin',
+    'copyright' => '广西师范大学教务处',
     'password' => '123456',
 ];
