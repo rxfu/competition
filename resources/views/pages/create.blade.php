afe349fdd5ce151f7c7e5dfd92dd18ef208df71e
Stephen Fu <stephen.frx@gmail.com>
2020-06-15 14:36:55 +0800

    修改本科教学时间点

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -126,7 +126,7 @@ $(function() {
     		up: 'fas fa-arrow-up',
     		down: 'fas fa-arrow-down'
     	},
-		maxDate: '2016-6-30'
+		maxDate: '2016-7-30'
 	});
     $('.datetimepicker').datetimepicker({
     	locale: 'zh-cn',
